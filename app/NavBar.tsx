542fb984485e9811a60c4452dc4c91c9b40b4b16
tseHaoSu <153491122+tseHaoSu@users.noreply.github.com>
2025-03-31 16:04:00 +1100

    setup prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { Gi3dHammer } from "react-icons/gi";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
-    { label: "Dashbaord", href: "/" },
+    { label: "Dashboad", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex flex-row space-x-4 border-b border-gray-300 p-4 items-center">
       <Gi3dHammer className="text-blue-500 text-2xl" />
-      <Link href="/" className="font-bold ">
+      <Link href="/" className="font-bold">
         IssueTracker
       </Link>
       <ul className="flex flex-row space-x-6">
@@ -18,7 +26,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "font-bold": link.href === currentPath,
+            })}
           >
             {link.label}
           </Link>
